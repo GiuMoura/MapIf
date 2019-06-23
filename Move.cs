e9bbf31f45204edacb1eb77bded6c8584b3232ce
diegog134 <47739448+diegog134@users.noreply.github.com>
2019-06-22 22:01:13 -0300

    scripts de sair de scene do unity e movimento

---
+++ Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float inputY;
+    public float inputX;
+    private Animator animator_;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        animator_=this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w")) {
+            inputY = 0;
+            inputX = 1; }
+            else if (Input.GetKey("a"))
+            {
+                inputX = 2;
+                inputY = 2;
+            }
+                else if (Input.GetKey("d"))
+                   {
+                     inputX = 2;
+                     inputY = 1;
+                    }
+        else
+        {
+            inputY = 0;
+            inputX = 0;
+        }
+                
+
+        animator_.SetFloat("Y", inputY);
+        animator_.SetFloat("X", inputX);
+    }
+}
