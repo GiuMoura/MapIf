caad53078b75786de8cb46f47306960c25d57478
diegog134 <47739448+diegog134@users.noreply.github.com>
2019-06-22 21:59:52 -0300

    script de manipulação do animator controler
    em termos gerais controla as animações

---
+++ moviment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moviment : MonoBehaviour
+{ public float vel=3;
+  public float velgi = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            if (Input.GetKey("a"))
+            {
+                transform.Rotate(0, velgi * Time.deltaTime, 0);
+            }
+            if (Input.GetKey("d"))
+            {
+                transform.Rotate(0, -(velgi * Time.deltaTime), 0);
+            }
+            transform.Translate(0,0,vel * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, velgi * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, -(velgi * Time.deltaTime), 0);
+        }
+    }
+}
