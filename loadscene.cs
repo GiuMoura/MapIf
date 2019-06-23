73f88a3002ce6e10b987ebb2f95cdd5f3a650e93
diegog134 <47739448+diegog134@users.noreply.github.com>
2019-06-22 21:57:24 -0300

    adição script primário para interações
    script que por meio de um colisor carrega uma scene que diz respeito ao objeto(no caso uma lousa)

---
+++ loadscene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class loadscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("COMPUTER");
+    }
+}
